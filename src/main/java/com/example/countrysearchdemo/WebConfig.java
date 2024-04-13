18bf54f90bffba2832d1f6345e175180614275d3
João Nuno Ferreira <joao.n.ferreira@linkconsulting.com>
2024-04-14 00:42:19 +0100

    CORS Config

--- src/main/java/com/example/countrysearchdemo/WebConfig.java
+++ src/main/java/com/example/countrysearchdemo/WebConfig.java
@@ -1,2 +1,18 @@
-package com.example.countrysearchdemo;public class WebConfig {
+package com.example.countrysearchdemo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://countrysearchdemo.onrender.com") // replace with your domain
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
